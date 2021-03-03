3372f3c90b653c7a857f61f9186d5a45e06775be
iy0kan <11053847+iy0kan@users.noreply.github.com>
2021-03-02 16:17:27 -0800

    initial commit

---
+++ Assets/KenKenScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KModkit;
+
+public class KenKenScript : MonoBehaviour {
+	public KMBombInfo Bomb;
+	public KMBombModule Module;
+	public KMAudio Audio;
+
+	public KMSelectable clearButton;
+	public KMSelectable submitButton;
+	public Renderer bounds;
+
+	private static int NEXT_ID = 1;
+	private int id;
+
+	void Awake() {
+		this.id = ++NEXT_ID;
+
+		AddButtonBehavior(clearButton);
+		AddButtonBehavior(submitButton);
+		clearButton.OnInteract += OnClear;
+		submitButton.OnInteract += OnSubmit;
+
+		Debug.Log(bounds.bounds);
+	}
+
+	void Log(string str) {
+		Debug.LogFormat(@"[KenKen {0}] {1}", this.id, str);
+	}
+
+	void AddButtonBehavior(KMSelectable btn) {
+		btn.OnInteract += delegate() {
+			StartCoroutine(ButtonPressAnimation(btn.transform));
+			Audio.PlayGameSoundAtTransform(
+				KMSoundOverride.SoundEffect.ButtonPress,
+				this.transform
+			);
+			return false;
+		};
+	}
+
+	IEnumerator ButtonPressAnimation(Transform loc) {
+		yield return null;
+	}
+
+	bool OnClear() {
+		Module.HandleStrike();
+		return false;
+	}
+
+	bool OnSubmit() {
+		Module.HandlePass();
+		return false;
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# KenKen Module
+
+## Changelog
